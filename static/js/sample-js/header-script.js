58f1f6a439dc9781f4d2c4dffeb5fcc66ae143b8
Abhishek Airan <cubicfammy@gmail.com>
2024-05-31 13:14:46 +0530

    commit 2
    Done:
    - created a responsive header
    To Do:
    - Making categories dynamic

--- static/js/sample-js/header-script.js
+++ static/js/sample-js/header-script.js
@@ -22,25 +22,59 @@ const addEventOnElem = function (elem, type, callback) {
  * navbar toggle
  */
 
-const navTogglers = document.querySelectorAll("[data-nav-toggler]");
-const navbar = document.querySelector("[data-navbar]");
-const navbarLinks = document.querySelectorAll("[data-nav-link]");
-const overlay = document.querySelector("[data-overlay]");
+// mobile menu variables
+const mobileMenuOpenBtn = document.querySelectorAll('[data-mobile-menu-open-btn]');
+const mobileMenu = document.querySelectorAll('[data-mobile-menu]');
+const mobileMenuCloseBtn = document.querySelectorAll('[data-mobile-menu-close-btn]');
 
-const toggleNavbar = function () {
-  navbar.classList.toggle("active");
-  overlay.classList.toggle("active");
+for (let i = 0; i < mobileMenuOpenBtn.length; i++) {
+
+  // mobile menu function
+  const mobileMenuCloseFunc = function () {
+    mobileMenu[i].classList.remove('active');
+  }
+
+  mobileMenuOpenBtn[i].addEventListener('click', function () {
+    mobileMenu[i].classList.add('active');
+  });
+
+  mobileMenuCloseBtn[i].addEventListener('click', mobileMenuCloseFunc);
+
 }
 
-addEventOnElem(navTogglers, "click", toggleNavbar);
 
-const closeNavbar = function () {
-  navbar.classList.remove("active");
-  overlay.classList.remove("active");
+
+
+
+// accordion variables
+const accordionBtn = document.querySelectorAll('[data-accordion-btn]');
+const accordion = document.querySelectorAll('[data-accordion]');
+
+for (let i = 0; i < accordionBtn.length; i++) {
+
+  accordionBtn[i].addEventListener('click', function () {
+
+    const clickedBtn = this.nextElementSibling.classList.contains('active');
+
+    for (let i = 0; i < accordion.length; i++) {
+
+      if (clickedBtn) break;
+
+      if (accordion[i].classList.contains('active')) {
+
+        accordion[i].classList.remove('active');
+        accordionBtn[i].classList.remove('active');
+
+      }
+
+    }
+
+    this.nextElementSibling.classList.toggle('active');
+    this.classList.toggle('active');
+
+  });
+
 }
-
-addEventOnElem(navbarLinks, "click", closeNavbar);
-
 
 
 /**
